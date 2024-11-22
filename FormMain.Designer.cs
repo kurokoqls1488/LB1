863b7d608c1bb82ce06e2a8008e05c958d32744a
kurokoqls1488 <gluk.dan@gmail.com>
2024-11-22 11:06:51 +0500

    Add files via upload

---
+++ FormMain.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace LB1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelTop = new Panel();
+            labelUsers = new Label();
+            panel2 = new Panel();
+            labelInfAboutUsers = new Label();
+            panelTop.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(labelUsers);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            panelTop.Location = new Point(0, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(10);
+            panelTop.Size = new Size(800, 100);
+            panelTop.TabIndex = 0;
+            // 
+            // labelUsers
+            // 
+            labelUsers.Dock = DockStyle.Fill;
+            labelUsers.Location = new Point(10, 10);
+            labelUsers.Name = "labelUsers";
+            labelUsers.Size = new Size(780, 80);
+            labelUsers.TabIndex = 0;
+            labelUsers.Text = "Список пользователей:";
+            labelUsers.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(labelInfAboutUsers);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 100);
+            panel2.Name = "panel2";
+            panel2.Padding = new Padding(10);
+            panel2.Size = new Size(800, 350);
+            panel2.TabIndex = 1;
+            // 
+            // labelInfAboutUsers
+            // 
+            labelInfAboutUsers.Dock = DockStyle.Fill;
+            labelInfAboutUsers.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            labelInfAboutUsers.Location = new Point(10, 10);
+            labelInfAboutUsers.Name = "labelInfAboutUsers";
+            labelInfAboutUsers.Size = new Size(780, 330);
+            labelInfAboutUsers.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panelTop);
+            Name = "FormMain";
+            Text = "Пользователи";
+            Load += FormMain_Load;
+            panelTop.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelTop;
+        private Panel panel2;
+        private Label labelUsers;
+        private Label labelInfAboutUsers;
+    }
+}
---
+++ User.cs
@@ -0,0 +1,11 @@
+﻿namespace LB1
+{
+    internal class User
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
