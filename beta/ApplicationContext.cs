4efc0281b916f75c87731de5387b69a8aa45e745
kurokoqls1488 <gluk.dan@gmail.com>
2024-11-22 11:14:33 +0500

    Add files via upload

---
+++ beta/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LB1
+{
+    internal class ApplicationContext: DbContext
+    {
+        public DbSet<User> Users { get; set; } = null;
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb2;Username=postgres;Password=123443211");
+        }
+    }
+}
