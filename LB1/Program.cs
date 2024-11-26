39c706da7cb425ea6fdef1c3b4c52ed3e5236826
Ильин Даниил <gluk.dan@gmail.com>
2024-11-26 13:43:42 +0500

    Данила

--- LB1/Program.cs
+++ LB1/Program.cs
@@ -16,7 +16,7 @@ namespace LB1
 
                 ApplicationConfiguration.Initialize();
             Application.Run(new FormMain());
-            //Ильин Даниил
+            //Ильин Данил
         }
     }
 }
